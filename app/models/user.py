0065303096f5c4d7b7d9024acf4537e3874762e4
mohammad <mo2215614@gmail.com>
2024-09-18 01:31:47 +0330

    add shell config in main , add set hash password and check password in model user

--- app/models/user.py
+++ app/models/user.py
@@ -3,20 +3,32 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from app import db
-
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 class User(db.Model):
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
-    username: orm.Mapped[str] = orm.mapped_column(sa.String(64), index=True, unique=True)
-    email: orm.Mapped[str] = orm.mapped_column(sa.String(64), index=True, unique=True)
+    username: orm.Mapped[str] = orm.mapped_column(
+        sa.String(64), index=True, unique=True)
+    email: orm.Mapped[str] = orm.mapped_column(
+        sa.String(64), index=True, unique=True)
     salt: orm.Mapped[Optional[str]] = orm.mapped_column(sa.String(256))
-    password_hash: orm.Mapped[Optional[str]] = orm.mapped_column(sa.String(256))
+    password_hash: orm.Mapped[Optional[str]
+                              ] = orm.mapped_column(sa.String(256))
 
-    posts : orm.WriteOnlyMapped['Post'] = orm.relationship('Post', back_populates='author')
+    posts: orm.WriteOnlyMapped['Post'] = orm.relationship( # type: ignore
+        'Post', back_populates='author')
 
-    create_at: orm.Mapped[datetime] = orm.mapped_column(sa.DateTime, default=datetime.utcnow)
-    update_at: orm.Mapped[datetime] = orm.mapped_column(sa.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    create_at: orm.Mapped[datetime] = orm.mapped_column(
+        sa.DateTime, default=datetime.utcnow)
+    update_at: orm.Mapped[datetime] = orm.mapped_column(
+        sa.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
         return f"<User(id={self.id}, username={self.username})>"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from app import app
+from app import app, db
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from app.models import user, post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'db': db, 'orm': orm, 'User': user.User, 'Post': post.Post}
