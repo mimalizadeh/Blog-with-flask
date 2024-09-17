adf70f8e29599663080562e4a14d1dbfe6ab503d
mohammad <mo2215614@gmail.com>
2024-09-18 03:07:11 +0330

    add check next page after login and redirect user to new page

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,8 @@
+from urllib.parse import urlsplit
+
 from app import app, db
 
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 
 from app.forms.login import LoginForm
 from app.models.user import User
@@ -24,7 +26,10 @@ def login():
             return redirect(url_for('login'))
 
         login_user(user_model, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
 
     return render_template("auth/login.html", title="Login", form=form)
 
